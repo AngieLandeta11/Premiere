9e1051a63c24ec0450aa5adbb9ffacbbdb4cda9e
Angelica <angelicalandeta11@gmail.com>
2022-05-14 19:36:08 -0500

    responsiva y login

---
+++ js/login.js
@@ -0,0 +1,6 @@
+function cambiar(){	
+	var link = document.getElementById('MiLink');
+	
+	var text = "Iniciar Sesion"; //Este texto reemplazara al contenido del elemento html
+	link.innerHTML = text;
+}
